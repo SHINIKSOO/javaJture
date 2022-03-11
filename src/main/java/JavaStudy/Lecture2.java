0efdf445f8de69e972453fd177d848e2719fcc5d
shiniksoo <soolism@daum.net>
2022-03-11 23:14:50 +0900

    nothing

--- src/main/java/JavaStudy/Lecture2.java
+++ src/main/java/JavaStudy/Lecture2.java
@@ -1,4 +1,26 @@
 package JavaStudy;
 
+import java.util.HashMap;
+
 public class Lecture2 {
+
+
+    HashMap<Integer,String> hashMap;
+    p
+
+
+    public static void main(String[] args){
+
+
+
+
+
+
+        /*
+        set은 순서가 없고, 중복을 허용하지 않는다.
+        List는 순서가있고, 중복을 허용한다. LInked는 물리적-논리적주소가 다르다?
+
+        MAP KEY-VALUE
+         */
+    }
 }
